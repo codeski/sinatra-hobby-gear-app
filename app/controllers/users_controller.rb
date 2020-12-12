848c260f6cb0d60cb9b87e040336be7b21c998da
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-12-12 15:54:08 -0700

    adding session and building user routes for signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        erb :"user/new"
+    end
+
+    post '/signup' do
+        user = User.create(params) 
+        session[:user_id] = user.id
+        redirect "/hobbies"
+    end
+    
     get '/login' do
         erb :"user/index"
     end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
 
     has_many :hobbies
+    has_many :items, through: :hobbies
 end
