24e62bfbe720aa46a0d585241748d150663ac9e5
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-12-11 17:21:42 -0700

    built seed file

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,4 +1,20 @@
-todd = User.create(name: "Todd", email: "tstoneberg@todd.com", password: "yo")
-User.create(name: "")
+todd = User.create(name: "Todd", email: "todd@todd.com", password: "yo")
+jill = User.create(name: "Jill", email: "jill@jill.com", password: "sup")
 
-Hobby.create(name: "skiing", season: "winter", user_id: todd.id)
+hobby1 = Hobby.create(name: "skiing", season: "winter", user_id: todd.id)
+hobby2 = Hobby.create(name: "fishing", season: "summer", user_id: todd.id)
+
+Item.create(name: "skis", year: 2005, company: "dps", hobbie_id: hobby1.id)
+Item.create(name: "boots", year: 2005, company: "dynafit", hobbie_id: hobby1.id)
+Item.create(name: "poles", year: 2005, company: "marker", hobbie_id: hobby1.id)
+
+Item.create(name: "pole", year: 2005, company: "ugly_stick", hobbie_id: hobby2.id)
+Item.create(name: "lure", year: 2005, company: "rapala", hobbie_id: hobby2.id)
+
+hobby3 = Hobby.create(name: "soccer", season: "spring", user_id: jill.id)
+hobby4 = Hobby.create(name: "dancing", season: "all", user_id: jill.id)
+
+Item.create(name: "kleets", year: 1996, company: "addidas", hobby_id: hobby3.id)
+Item.create(name: "ball", year: 1990, company: "nike", hobby_id: hobby3.id)
+
+Item.create(name: "shoes", year: 2019, company: "sketchers", hobby_id: hobby4.id)
