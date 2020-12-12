a5c8875af714a983d02af826ef91b3cf2a760eeb
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-12-11 19:33:00 -0700

    building RESTful convention for hobby

--- app/controllers/hobby_controller.rb
+++ app/controllers/hobby_controller.rb
@@ -1,7 +1,30 @@
 class HobbyController < Sinatra::Base
   
-    get "/" do
-      
+    get "/hobby" do
+   
+    end
+
+    get "/hobby/:id" do
+
     end
   
+    get "/hobby/new" do
+
+    end
+
+    post "/hobby" do
+
+    end
+
+    get "/hobby/:id/edit" do
+
+    end
+
+    patch "/hobby/:id"
+
+    end
+
+    delete "/hobby/:id"
+
+    end
   end
