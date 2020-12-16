a56f57e450960ceb10c600ddb14caea6b1606b2a
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-12-15 21:22:45 -0700

    modified spacing in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
         user = User.new(params)
         if  user.save
             session[:user_id] = user.id
+
             redirect "/hobbies" 
         else 
             @errors = user.errors.full_messages
+
             erb :"user/new"
         end
     end
@@ -23,15 +25,18 @@ class UsersController < ApplicationController
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
+
             redirect "/hobbies"
         else
             @error = "Wrong Email and/or Password"
+
             erb :'user/login'
         end 
     end
 
     get '/logout' do
         session.clear
+        
         redirect 'login'
     end
 end
